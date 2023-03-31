2f3c5e8c21fe33eabde36aafedc9fd778ee1812f
Steve Scammon <stevescammon@me.com>
2023-03-31 15:30:29 -0400

    autocommit

--- bd9.py
+++ bd9.py
@@ -1,4 +1,4 @@
-import sys
+import subprocess
 import os
 import datetime
 import pyperclip
@@ -17,9 +17,9 @@ def increment_date(date):
     return date + datetime.timedelta(days=1)
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='bd9: Copy a github commit with a date then increment the date.')
-    parser.add_argument('string', help='The text to copy with the date.')
-    parser.add_argument('--start', metavar='MM-DD-YYYY', help='Set the initial date (format: MM-DD-YYYY).')
+    parser = argparse.ArgumentParser(description='bd9: Runs a backdated github commit then increments the date on each run.')
+    parser.add_argument('message', help='The commit message.')
+    parser.add_argument('--start', metavar='YYYY-MM-DD', help='Set the initial date.')
     return parser.parse_args()
 
 def main():
@@ -29,18 +29,18 @@ def main():
     backdater_path = os.path.join(home_dir, '.bd9')
 
     if args.start:
-        start_date = datetime.datetime.strptime(args.start, '%m-%d-%Y').date()
+        start_date = datetime.datetime.strptime(args.start, '%Y-%m-%d').date()
         write_date_to_file(backdater_path, start_date)
     elif not os.path.exists(backdater_path):
         today = datetime.datetime.now().date()
         write_date_to_file(backdater_path, today)
 
     date = read_date_from_file(backdater_path)
-    text_with_date = f'git commit -m "{args.string}" --date "{date.strftime("%m-%d-%Y")}"'
-    pyperclip.copy(text_with_date)
-    print(f'Copied to clipboard: {text_with_date}')
-
+    text_with_date = f'git commit -m "{args.message}" --date \"{date.strftime("%Y-%m-%d")}\"'
+    cmd_args = text_with_date.split()
+    subprocess.run(cmd_args, shell=True)
     next_date = increment_date(date)
+    print(f'Committed for: {date.strftime("%Y-%m-%d")}. Next commit will be for: {next_date.strftime("%Y-%m-%d")}')
     write_date_to_file(backdater_path, next_date)
 
 if __name__ == '__main__':
